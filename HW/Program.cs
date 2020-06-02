62df1ed91cad90ebaa50b8bfccef267c37446fbc
UID3180102751 <3180102751@zju.edu.cn>
2020-06-02 15:43:34 +0800

    kk

--- HW/Program.cs
+++ HW/Program.cs
@@ -19,13 +19,9 @@ namespace HW
 
 
         {
+
             Application.EnableVisualStyles();
-
-
             Application.SetCompatibleTextRenderingDefault(false);
-
-
-
             ESRI.ArcGIS.RuntimeManager.Bind(ESRI.ArcGIS.ProductCode.EngineOrDesktop);
             Application.Run(new Form1());
 
